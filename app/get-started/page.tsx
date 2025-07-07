7b5427cafabbfe20f37d988dec3c751e6bc534e2
Alexandra Sandoval <alexandoval13@gmail.com>
2025-07-06 23:25:57 -0700

    Connect Supabase with auth and boilerplate

---
+++ app/get-started/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+const GetStarted = () => {
+  return (
+    <div>
+      <main className="p-6">
+        <h1 className="text-3xl font-bold">Welcome to Mantis</h1>
+        <div className="flex flex-row gap-x-6 ">
+          <p className="text-gray-600 mt-2">Build your first bubble map.</p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default GetStarted;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Home from './home/page';
+
+export default async function App() {
+  <div>
+    <Home />
+  </div>;
+}
